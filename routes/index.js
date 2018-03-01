c573c75bd37ea0fb0298493986f6ff768f12c947
James <jwin4740@gmail.com>
2018-02-28 19:52:29 -0500

    added hamilton route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hamilton', function(req, res, next) {
+  res.render('hamilton');
+});
+
 module.exports = router;
